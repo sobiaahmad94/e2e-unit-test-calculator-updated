64c4293d62f659927c5ec19a061a57184c5d3cc6
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-05-24 00:45:11 +0100

    more tests written

--- react_calculator/src/tests/Calculator.test.js
+++ react_calculator/src/tests/Calculator.test.js
@@ -31,10 +31,11 @@ describe('Calculator', () => {
   });
 
 
-    // A) I think this one just checks that if a number is select (e.g. 4) then the running-total updates
+  // A) I think this one just checks that if a number is select (e.g. 4) then the running-total updates
   it('should change running total on number enter', () => {
     const button4 = container.getByTestId('number4');
     const runningTotal = container.getByTestId('running-total');
+
     fireEvent.click(button4);
     expect(runningTotal.textContent).toEqual('4');
   });
@@ -47,13 +48,14 @@ describe('Calculator', () => {
     const operatorAdd = container.getByTestId('operator-add');
     const operatorEquals = container.getByTestId('operator-equals');
     const runningTotal = container.getByTestId('running-total');
-      fireEvent.click(button1);
-      fireEvent.click(operatorAdd);
-      fireEvent.click(button4);
-      fireEvent.click(operatorEquals);
-      // textContent is a JS thing which is just what's inside of a tag
-      expect(runningTotal.textContent).toEqual('5');
-    });
+
+    fireEvent.click(button1);
+    fireEvent.click(operatorAdd);
+    fireEvent.click(button4);
+    fireEvent.click(operatorEquals);
+    // textContent is a JS thing which is just what's inside of a tag
+    expect(runningTotal.textContent).toEqual('5');
+  });
 
 
   // 2) subtract 4 from 7 and get 3
@@ -63,12 +65,71 @@ describe('Calculator', () => {
     const operatorSubtract = container.getByTestId('operator-subtract');
     const operatorEquals = container.getByTestId('operator-equals');
     const runningTotal = container.getByTestId('running-total');
+
     fireEvent.click(button7);
     fireEvent.click(operatorSubtract);
     fireEvent.click(button4);
     fireEvent.click(operatorEquals);
     expect(runningTotal.textContent).toEqual('3');
   });
+
+
+  // 3) multiply 3 by 5 and get 15
+  it('should multiply 3 by 5 to get 15', () => {
+    const button3 = container.getByTestId('number3');
+    const button5 = container.getByTestId('number5');
+    const operatorMultiply = container.getByTestId('operator-multiply');
+    const operatorEquals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+
+    fireEvent.click(button3);
+    fireEvent.click(operatorMultiply)
+    fireEvent.click(button5);
+    fireEvent.click(operatorEquals);
+    expect(runningTotal.textContent).toEqual('15');
+  });
+
+  // 4) divide 21 by 7 and get 3
+  it('should divide 21 by 7 and get 3', () => {
+    // get 2 and 1
+    // create a button21 variable and convert button1 and button2 into strings
+    const button2 = container.getByTestId('number2');
+    const button1 = container.getByTestId('number1')
+    // const button21 = str(button1) + str(button2)
+    const operatorDivide = container.getByTestId('operator-divide');
+    const button7 = container.getByTestId('number7');
+    const operatorEquals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+
+    fireEvent.click(button2);
+    fireEvent.click(button1);
+    fireEvent.click(operatorDivide);
+    fireEvent.click(button7);
+    fireEvent.click(operatorEquals);
+    expect(runningTotal.textContent).toEqual('3');
+  });
+
+  // 5) concatenate (merge) multiple number button clicks (3 and 4 should be 34 and not 7 lol, kinda like I was thinking for the one above as I converted then to strings)
+  // 34 (clicking 3 and 4)
+  // if statement if 3 and 4 are clicked right after each other store it as 34
+  // actually might be an easier way as if they click two numbers then they should be concatenated
+  it('should concatenate multiple numbers together', () =>{
+    const button3 = container.getByTestId('number3');
+    const button4 = container.getByTestId('number4');
+    // const button34 = str(button3) + str(button4);
+    const runningTotal = container.getByTestId('running-total');
+
+    fireEvent.click(button3);
+    fireEvent.click(button4);
+    expect(runningTotal.textContent).toEqual('34');
+  
+
+
+  });
+
+
+
+
 });
 
 
@@ -89,31 +150,14 @@ describe('Calculator', () => {
 
 
 
-
-
-
-
-
-
-//   // 2)
-
-//   // 3)
-
-//   // 4)
-
-//   // 5)
-
-//   // 6)
-
-//   // 7)
-
-//   // 1) calculator.add() - add 1 to 4 and get 5
-//   // 2) calculator.subtract() subtract 4 from 7 and get 3
-//   // 3) calculator.multiply() - multiply 3 by 5 and get 15
-//   // 4) calculator.divide() - divide 21 by 7 and get 3
-//   // 5) calculator.numberClick() - concatenate multiple number button clicks
-//   // 6) calculator.operatorClick() - chain multiple operations together
-//   // 7) calculator.clearClick() - clear the running total without affecting the calculation
+// Tests I wanna do:
+  // 1) calculator.add() - add 1 to 4 and get 5 [done]
+  // 2) calculator.subtract() subtract 4 from 7 and get 3 [done]
+  // 3) calculator.multiply() - multiply 3 by 5 and get 15 [doneee]
+  // 4) calculator.divide() - divide 21 by 7 and get 3 [doneeeeeee]
+  // 5) calculator.numberClick() - concatenate multiple number button clicks
+  // 6) calculator.operatorClick() - chain multiple operations together
+  // 7) calculator.clearClick() - clear the running total without affecting the calculation
 
 
 
