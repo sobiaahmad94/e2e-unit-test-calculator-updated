ac93e02ffac971a3894a2d7244783de2ef81d35c
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-05-24 02:13:32 +0100

    some e2e tests written

--- react_calculator/src/tests/Calculator.cy.js
+++ react_calculator/src/tests/Calculator.cy.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Calculator from '../containers/Calculator';
 import KeyPad from '../components/KeyPad';
 import { render, fireEvent } from '@testing-library/react';
+
 
 // WEE NOTES:
 // ids need to be put in like: data-testid="number7"
@@ -18,6 +19,7 @@ import { render, fireEvent } from '@testing-library/react';
 // describe lets you label the component name so Calculator is labelled here
 // then there's an arrow function
 describe('Calculator', () => {
+
   let container;
   // the container variable here to store a version of Calculator in the test
   // its just easier to access it by using container as a variable to reference the calculator
@@ -27,9 +29,11 @@ describe('Calculator', () => {
   // I think beforeEach is kinda like when we used setUp in Python
   // I think beforeEach makes it that it's rendered every time I try to check for a wee test just so it's ready but not 100%
   beforeEach(() => {
+    cy.visit('http://localhost:3000');
     container = render(<Calculator />);
   });
 
+  ///////// UNIT TESTING USING THE REACT TESTING LIBRARY
 
   // A) I think this one just checks that if a number is select (e.g. 4) then the running-total updates
   it('should change running total on number enter', () => {
@@ -113,7 +117,7 @@ describe('Calculator', () => {
   // 34 (clicking 3 and 4)
   // if statement if 3 and 4 are clicked right after each other store it as 34
   // actually might be an easier way as if they click two numbers then they should be concatenated
-  it('should concatenate multiple numbers together when they are clicked', () =>{
+  it('should concatenate multiple numbers together when they are clicked', () => {
     const button3 = container.getByTestId('number3');
     const button4 = container.getByTestId('number4');
     // const button34 = str(button3) + str(button4);
@@ -167,7 +171,7 @@ describe('Calculator', () => {
   //   const operatorSubtract = container.getByTestId('operator-subtract');
   //   const button2 = container.getByTestId('number2');
   //   const runningTotal = container.getByTestId('running-total');
-  
+
   //   fireEvent.click(button7);
   //   fireEvent.click(operatorAdd);
   //   fireEvent.click(button1);
@@ -176,62 +180,40 @@ describe('Calculator', () => {
   //   fireEvent.click(runningTotal);
   //   expect(runningTotal.textContent).toEqual('2');
   // });
-  
-    // 7) clear the running total without affecting the calculation
-    // maybe I could do 1 + 2 = 3 so can check that and then set it to 0 
-    // omg actually could use useState for this
-    // STUCK ON THISSSS :S will ask tomorrow.
-    it('should be able to clear the running total without affecting the calculation', () => {
 
-      // useState (total is set to 0)
-      const [total, setTotal] = useState(0)
+  // 7) clear the running total without affecting the calculation
+  // maybe I could do 1 + 2 = 3 so can check that and then set it to 0 
+  // omg actually could use useState for this
+  // STUCK ON THISSSS :S will ask tomorrow.
+  it('should be able to clear the running total without affecting the calculation', () => {
 
-      const button1 = container.getByTestId('number1');
-      const operatorAdd = container.getByTestId('operator-add');
-      const button2 = container.getByTestId('number2');
-      const operatorEquals = container.getByTestId('operator-equals');
-      const button3 = container.getByTestId('number3');
-      const runningTotal = container.getByTestId('running-total');
+    // useState (total is set to 0)
+    const [total, setTotal] = useState(0)
 
-
-      
-      fireEvent.click(button1);
-      fireEvent.click(operatorAdd);
-      fireEvent.click(button2);
-      fireEvent.click(operatorEquals);
-      fireEvent.click(button3);
-      fireEvent.click(runningTotal);
-      
-      setTotal((runningTotal.textContent))
-      expect(runningTotal.textContent).toEqual('0');
-
-    });
+    const button1 = container.getByTestId('number1');
+    const operatorAdd = container.getByTestId('operator-add');
+    const button2 = container.getByTestId('number2');
+    const operatorEquals = container.getByTestId('operator-equals');
+    const button3 = container.getByTestId('number3');
+    const runningTotal = container.getByTestId('running-total');
 
 
 
+    fireEvent.click(button1);
+    fireEvent.click(operatorAdd);
+    fireEvent.click(button2);
+    fireEvent.click(operatorEquals);
+    fireEvent.click(button3);
+    fireEvent.click(runningTotal);
 
+    setTotal((runningTotal.textContent))
+    expect(runningTotal.textContent).toEqual('0');
 
-});
-
-
-
-
-
-
-
-
-
-
-
+  });
 
 
 
-
-
-
-
-
-// Tests I wanna do:
+  // unit tests I wanna do:
   // 1) calculator.add() - add 1 to 4 and get 5 [done]
   // 2) calculator.subtract() subtract 4 from 7 and get 3 [done]
   // 3) calculator.multiply() - multiply 3 by 5 and get 15 [doneee]
@@ -241,6 +223,85 @@ describe('Calculator', () => {
   // 7) calculator.clearClick() - clear the running total without affecting the calculation
 
 
+  ///////// e2e TESTING USING CYPRESS:
+
+  // e2e tests I wanna do:
+  // 1) Do the number buttons update the display of the running total? [doneee]
+  // 2) Do the arithmetical operations update the display with the result of the operation? [doneeee]
+  // E.g. does 2 + 2 - update the display to 4
+  // 3) Can multiple operations be chained together?
+  // E.g. does 3 + 1 - 2 == 2
+  // 4) Is the output as expected for positive numbers
+  // 5) Is the output as expected for negative numbers
+  // 6) Is the output as expected for decimal numbers
+  // 7) Is the output as expected for very large numbers
+  // 8) What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
+
+  // SOME NOTES:
+  // cy.get is like grabbing an element I think (similar to querySelector)
+  // instead of number1, number2 etc as the ids like in unit testing
+  // have to grab the full thing like data-testid="number1"
+  // .click can be used as a click event similar to before
+  // .should is the equivalent to expected
+  // have.text is mentioned here in the cypress documentation: example was this - .should('have.text', 'I love testing')
+
+
+  // 1) Do the number buttons update the display of the running total?
+  it('Should update the display of the running total', () => {
+  
+    // click events on the numbers
+    cy.get('[data-testid="number1"]').click();
+    cy.get('[data-testid="number7"]').click();
+    cy.get('[data-testid="number2"]').click();
+    cy.get('[data-testid="number4"]').click();
+    cy.get('[data-testid="number3"]').click();
+    cy.get('[data-testid="number0"]').click();
+    cy.get('[data-testid="number8"]').click();
+
+    // should is basically like expected I think 
+    // have.text needs to be used
+    cy.get('[data-testid="running-total"]').should('have.text', '1724308');
+  });
+
+    // 2) Do the arithmetical operations update the display with the result of the operation?
+    // should be something like 3 - 2 
+    it('Should update the display when clicking arithmetical operations when entering sums', () => {
+  
+      // click events on the button ids on the calc
+      cy.get('[data-testid="number3"]').click();
+      cy.get('[data-testid="operator-subtract"]').click();
+      cy.get('[data-testid="number1"]').click();
+      cy.get('[data-testid="equals-operator"]').click();
+      
+      // should = expected
+      // have.text again:)
+      cy.get('[data-testid="running-total"]').should('have.text', '2');
+    });
+
+      // 3) Can multiple operations be chained together?
+      // could be something like 4 + 1 - 2 == 3
+
+      it('Should be able to let you chain multiple arithmetic operators together', () => {
+  
+        // click events on the button ids on the calc
+        cy.get('[data-testid="number4"]').click();
+        cy.get('[data-testid="operator-add"]').click();
+        cy.get('[data-testid="number1"]').click();
+        cy.get('[data-testid="operator-subtract"]').click();
+        cy.get('[data-testid="number2"]').click();
+        cy.get('[data-testid="equals-operator"]').click();
+
+
+        // should = expected
+        // have.text again:)
+        cy.get('[data-testid="running-total"]').should('have.text', '3');
+      });
+
+
+
+
+
+});
 
 
 
